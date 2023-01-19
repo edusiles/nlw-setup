81847e3c22efb1688539904a941ca69abab4d7bb
Eduardo Siles <eduardosiles2003@gmail.com>
2023-01-19 17:14:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Selecionar o formulário em uma variável
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
+  takePills: ["01-03"],
+  journal: ["01-02"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
