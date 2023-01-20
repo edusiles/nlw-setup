626fafc013abdea777d58099a35b2bdc63dc8114
Eduardo Siles <eduardosiles2003@gmail.com>
2023-01-19 21:59:35 -0300

    update javascript

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 // Selecionar o formulário em uma variável
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
+// Selecionar botão na variável
+const button = document.querySelector('header button')
+
+// Verificar se o botão foi clicado
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+// Função de adicionar dia
+function add() {
+  // Variável com a data atual a ser adicionada
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+  // Verificar se o dia já existe
+  const dayExists = nlwSetup.dayExists(today)
+  
+  // Teste condicional com base na existência do dia
+  if (dayExists) {
+    alert("🚨 Dia já adicionado! 🛑")
+
+    // Finalizar a função
+    return
+  }
+
+  // Adicionar o dia
+  nlwSetup.addDay(today)
+  alert("Dia adicionado com sucesso! ✅")
 }
 
+// Função para salvar dia
+function save() {
+  // Guardando dia no armazenamento local
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+// Transformando data de volta em objeto e adicionando na variável
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+
+// Carregar data na página
 nlwSetup.setData(data)
 nlwSetup.load()
